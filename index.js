7277c8e40125efb21a1f3689607da67bf05f96ca
github-marynapoliarush <merienf8118@gmail.com>
2023-01-23 16:53:42 +0200

    first page done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 
 
-async function getCodewarsData(){
-    const url = 'https://www.codewars.com/api/v1/users/rsschool_a5963a1d311bb850';
-    const res = await fetch(url);
-    const data = await res.json(); 
-    console.log(data.ranks.overall.score)
-    let x= data.ranks.overall.score
-    document.querySelector('body').textContent = `${x}`
-}
-getCodewarsData()
+// async function getCodewarsData(){
+//     const url = 'https://www.codewars.com/api/v1/users/rsschool_a5963a1d311bb850';
+//     const res = await fetch(url);
+//     const data = await res.json(); 
+//     console.log(data.ranks.overall.score)
+//     let x= data.ranks.overall.score
+//     document.querySelector('body').textContent = `${x}`
+// }
+// getCodewarsData()
+
+
+
+let navBar = document.getElementById('nav');
+console.log(navBar)
+
+
+navBar.addEventListener('mouseover',function(){
+    
+    document.querySelectorAll('.nav-item-text').forEach( item =>{
+        
+        item.style.display = 'block'
+        navBar.classList.add('active')
+    })
+    
+})
+
+navBar.addEventListener('mouseout',function(){
+   
+    document.querySelectorAll('.nav-item-text').forEach( item =>{
+        
+        item.style.display = 'none'
+        navBar.classList.remove('active')
+    })
+    
+})
