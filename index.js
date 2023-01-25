c8e9d32a8cde564996565d0c900b2a4b565a12b3
github-marynapoliarush <merienf8118@gmail.com>
2023-01-25 12:39:20 +0200

    third page dev

--- index.js
+++ index.js
@@ -1,67 +1,97 @@
+//codewars parsing
+
+async function getCodewarsData(){
+    const url = 'https://www.codewars.com/api/v1/users/rsschool_a5963a1d311bb850';
+    const res = await fetch(url);
+    const data = await res.json(); 
+    // console.log(data)
+    let rank= data.ranks.overall.name;
+    let tasksSolved = data.codeChallenges.totalCompleted;
+    document.querySelector('.ku').textContent = `${rank}`
+    document.querySelector('.tasksnum').textContent = `${tasksSolved}`
+}
+getCodewarsData()
 
 
-// async function getCodewarsData(){
-//     const url = 'https://www.codewars.com/api/v1/users/rsschool_a5963a1d311bb850';
-//     const res = await fetch(url);
-//     const data = await res.json(); 
-//     console.log(data.ranks.overall.score)
-//     let x= data.ranks.overall.score
-//     document.querySelector('body').textContent = `${x}`
-// }
-// getCodewarsData()
 
 
+// nav bar movements
 
 let navBar = document.getElementById('nav');
 
-navBar.addEventListener('mouseover',function(){
-    
+navBar.addEventListener('mouseover',function(){ 
     document.querySelectorAll('.nav-item-text').forEach( item =>{
-        
         item.style.display = 'block'
         navBar.classList.add('active')
     })
-    
 })
 
 navBar.addEventListener('mouseout',function(){
-   
     document.querySelectorAll('.nav-item-text').forEach( item =>{
-        
         item.style.display = 'none'
         navBar.classList.remove('active')
     })
-    
 })
 
 
-// let aboutY = document.querySelector('.about-first-text').offsetTop
-// console.log(aboutY)
-// window.addEventListener("scroll",function(){
-//     console.log(window.scrollY+100)
-//     if((window.scrollY+600) == aboutY){
-//         document.querySelector('.about-first-text').style.animation = 'mainHeading2 1s ease-in-out'
-//     }
-// })
 
 
 
-let text = document.querySelector('.about-first-text')
-let textTop = text.getBoundingClientRect().top
+//text appearence
+
+let texts = document.querySelectorAll('.text-block')
 let windowheigth= window.innerHeight;
-console.log(textTop)
-console.log(windowheigth)
 
-if (windowheigth>textTop+200){
-    console.log('logged')
-    text.classList.add('about-first-text-active')
+for ( const textBlock of texts){
+    let textTop = textBlock.getBoundingClientRect().top
+    
+    // console.log(textTop)
+    // console.log(windowheigth)
+    
+    if (windowheigth>textTop+200){
+        // console.log('logged')
+        textBlock.classList.add('text-block-active')
+    }
+    
+    window.addEventListener("scroll",function(){
+    // console.log(textTop-windowheigth)
+    // console.log(window.pageYOffset)
+        if ( window.pageYOffset > textTop-windowheigth+200){
+            // console.log('logged')
+            textBlock.classList.add('text-block-active')
+        }
+    })
 }
 
-window.addEventListener("scroll",function(){
-console.log(textTop-windowheigth)
-console.log(window.pageYOffset)
-    if ( window.pageYOffset > textTop-windowheigth+200){
+
+
+
+//list item appearence
+
+function appearence(item,i){
+    setTimeout(function(){
         console.log('logged')
-        text.classList.add('about-first-text-active')
+        item.classList.add('course-active')
+    } , i*500)
+}
+
+let courses = document.querySelectorAll('.course')
+let educationBlockHeight=document.querySelector('.education-second').getBoundingClientRect().top
+console.log(courses)
+
+for( let i=0; i<courses.length;i++){
+    if (windowheigth>educationBlockHeight+200){
+        // console.log('logged')
+        appearence(courses[i],i)
     }
-})
+    
+    window.addEventListener("scroll",function(){
+    // console.log(textTop-windowheigth)
+    // console.log(window.pageYOffset)
+        if ( window.pageYOffset > educationBlockHeight-windowheigth+200){
+            // console.log('logged')
+            appearence(courses[i],i)
+        }
+    })
+    
+}
